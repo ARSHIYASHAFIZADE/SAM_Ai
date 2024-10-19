f2e5f003accedf3500286d984aeec7a68d2c984a
ARSHIYASHAFIZADE <145537704+ARSHIYASHAFIZADE@users.noreply.github.com>
2024-10-20 04:42:29 +0800

    Update App.tsx

--- sam/src/App.tsx
+++ sam/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
 import Home from './Home';
 import Contact from './Contact';
