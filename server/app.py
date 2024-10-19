9decb1721956d20739c22abb6f558c073e04b8df
ARSHIYASHAFIZADE <145537704+ARSHIYASHAFIZADE@users.noreply.github.com>
2024-10-20 05:16:52 +0800

    Update app.py

--- server/app.py
+++ server/app.py
@@ -643,6 +643,5 @@ def detect_breast_cancer():
         print(f"Exception occurred: {e}")
         return jsonify({"error": str(e)}), 400
 
-if __name__ == '__main__':
-    app.run()
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
